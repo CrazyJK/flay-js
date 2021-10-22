0ee563be845dcb2f6cd991b0914284f5ba5dac16
Jongkwan Nam <jongkwan.nam@gmail.com>
2021-10-22 10:04:17 +0900

    lecture

--- app.js
+++ app.js
@@ -7,11 +7,37 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// ref. http://expressjs.com/en/4x/api.html
 var app = express();
+
+/*
+	app.locals
+		자바스크립트 객체이고, 프로퍼티들은 애플리케이션 내의 지역 변수들이다.
+		애플리케이션의 라이프 타임 동안 유효하다.
+	req.app.locals
+		미들웨어에서 app의 지역 변수들을 사용할 수 있게 해준다.
+	res.locals
+		request의 라이프 타임 동안에만 유효하다.
+		html/view 클라이언트 사이드로 변수들을 보낼 수 있으며, 그 변수들은 오로지 거기서만 사용할 수 있다.
+*/
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// 미들웨어 함수
+var myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
+};
+app.use(myLogger);
+
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now();
+  next();
+};
+app.use(requestTime);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +47,63 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/middleware', function (req, res) {
+  var responseText = 'Hello World!';
+  responseText += 'Requested at: ' + req.requestTime + '';
+  res.send(responseText);
+});
+
+// add my controller
+app.get("/new-entry", function (req, res) {
+	res.render("new-entry");
+});
+
+app.post("/new-entry", function (req, res) {
+	if (!req.body.title || !req.body.body) {
+		res.status(400).send("Entries must have a title and a body.");
+		return;
+	}
+	entries.push({
+		title: req.body.title,
+		body: req.body.body,
+		published: new Date()
+	});
+	res.redirect("/");
+});
+
+// path variables
+app.get('/topic/:id/:mode', function (req, res) {
+	// 라우터 경로의 변경 /:id/:mode 를 통해 path 방식 url 값을 가져올 수 있다.
+	var topic = [
+		'javascript is...',
+		'nodejs is...',
+		'express is...'
+	];
+	var li = `
+		<li><a href="/topic/0">js</a></li>
+		<li><a href="/topic/1">nodejs</a></li>
+		<li><a href="/topic/2">express</a></li>
+		`
+	res.send(li + '<br>' + topic[req.params.id] + req.params.mode);
+	//path 방식을 사용하는 url의 경우 params를 통해서 값을 가져올 수 있음
+});
+
+// query
+app.get('/topic', function(req, res){
+	var topic = [
+		'javascript is...',
+		'nodejs is...',
+		'express is...'
+	];
+	var li = `
+	<li><a href="/topic?id=0">js</a></li>
+	<li><a href="/topic?id=1">nodejs</a></li>
+	<li><a href="/topic?id=2">express</a></li>
+	`
+  // 선택한 링크에 따라서 다른 정보를 출력하는 동적인 애플리케이션의 기본골격
+	res.send(li + '<br>' + topic[req.query.id]);
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
