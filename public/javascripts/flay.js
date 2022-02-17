1c3df715a4ee088e346390bd46b9b0e9c221d2df
jongkwan-nam <jongkwan.nam@gmail.com>
2022-02-17 18:05:38 +0900

    enhance

--- public/javascripts/flay.js
+++ public/javascripts/flay.js
@@ -1,8 +1,20 @@
-fetch('/flayground/list')
-	.then((res) => res.json())
-	.then((list) => {
-		console.log('list', list);
-		list.forEach((item) => {
-			$('#flayList').append(`<li>${item}</li>`);
-		});
+// fetch('/flay/list')
+// 	.then((res) => res.json())
+// 	.then((list) => {
+// 		console.log('list', list.length);
+// 		renderList(list);
+// 	});
+
+function renderList(list) {
+	list.forEach((item) => {
+		$('#flayList').append(`<li>${item}</li>`);
 	});
+}
+
+$.ajax({
+	url: '/flay/list',
+	success: (list) => {
+		console.log('list', list.length);
+		renderList(list);
+	},
+});
