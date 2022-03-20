cac9d7deac959cce53eb9cb88ed583ecb5607c4d
jk kamoru <Crazy.4.JK@gmail.com>
2022-03-20 18:34:30 +0900

    enhance

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -2,33 +2,35 @@
  * layout js
  */
 
+const Layout = {
+	setActiveMenu: () => {
+		$('#navbarNavAltMarkup .nav-link').removeClass('active').attr('aria-current', '');
+		$('#navbarNavAltMarkup .nav-link[href="' + location.pathname + '"]')
+			.addClass('active')
+			.attr('aria-current', 'page');
+	},
+	setThemeToggle: () => {
+		$('#themeToggler').on('change', () => {
+			const isDarkTheme = $('#themeToggler').prop('checked');
+			$('#themeToggler ~ label').html(isDarkTheme ? 'Dark' : 'Light');
+			$('html').attr('data-theme', isDarkTheme ? 'dark' : 'light');
+			LocalStorageItem.set('flayground.theme', isDarkTheme);
+		});
+
+		const theme = LocalStorageItem.getBoolean('flayground.theme', false);
+		$('#themeToggler').prop('checked', theme).trigger('change');
+	},
+	setStylesheet: () => {
+		let styleCssName = location.pathname;
+		if (location.pathname.charAt(location.pathname.length - 1) === '/') {
+			styleCssName += 'index';
+		}
+		$('head').append(`<link rel="stylesheet" href="/stylesheets${styleCssName}.css">`);
+	},
+};
+
 $(document).ready(() => {
-	setActiveMenu();
-	setThemeToggle();
+	Layout.setStylesheet();
+	Layout.setActiveMenu();
+	Layout.setThemeToggle();
 });
-
-function setActiveMenu() {
-	$('#navbarNavAltMarkup .nav-link').removeClass('active').attr('aria-current', '');
-	$('#navbarNavAltMarkup .nav-link[href="' + location.pathname + '"]')
-		.addClass('active')
-		.attr('aria-current', 'page');
-}
-
-function setThemeToggle() {
-	$('#themeToggler').on('change', () => {
-		const isDarkTheme = $('#themeToggler').prop('checked');
-		$('html').attr('data-theme', isDarkTheme ? 'dark' : 'light');
-		$('#themeToggler ~ label').html(isDarkTheme ? 'Dark' : 'Light');
-		LocalStorageItem.set('flayground.theme', isDarkTheme);
-	});
-
-	const theme = LocalStorageItem.getBoolean('flayground.theme', false);
-	$('#themeToggler').prop('checked', theme).trigger('change');
-}
-
-console.log('pathname', location.pathname);
-let styleCssName = location.pathname;
-if (location.pathname.charAt(location.pathname.length - 1) === '/') {
-	styleCssName += 'index';
-}
-$('head').append(`<link rel="stylesheet" href="/stylesheets${styleCssName}.css">`);
